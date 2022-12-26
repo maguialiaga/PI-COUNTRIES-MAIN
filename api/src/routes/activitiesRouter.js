e26263404177c822056b99a472734adf52a8b6e3
maguialiaga <maguialiaga1@gmail.com>
2022-12-26 12:21:06 -0300

    activitiesRouter changes

--- api/src/routes/activitiesRouter.js
+++ api/src/routes/activitiesRouter.js
@@ -1,13 +1,24 @@
 const { Router } = require("express");
-const { createActivity } = require("../controllers/activitiesController");
+const {
+  createActivity,
+  getActivities,
+  deleteActivity,
+  updateActivity,
+} = require("../controllers/activitiesController");
 const { validateAct } = require("../middlewares/index");
 
 const activitiesRouter = Router();
 
 activitiesRouter.post("/", validateAct, createActivity); //le agrego el middleware en el medio para antes de que entre al controller
+activitiesRouter.get("/", getActivities);
+activitiesRouter.delete("/:id", deleteActivity);
+activitiesRouter.put("/edit/:id", updateActivity);
 
 module.exports = activitiesRouter;
 
 //POST
 // Recibe los datos recolectados desde el formulario controlado de la ruta de creación de actividad turística por body
 // Crea una actividad turística en la base de datos, relacionada con los países correspondientes
+
+//GET ---> que me traiga todas las actividades!
+//DELETE ---> que me elimine la actividad!
