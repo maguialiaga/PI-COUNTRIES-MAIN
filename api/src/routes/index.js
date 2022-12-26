40787afa670d8e4ebbed059d63d2f069fc7aaf5c
maguialiaga <maguialiaga1@gmail.com>
2022-12-25 23:58:52 -0300

    inyect middlewares and routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const express = require("express");
+const morgan = require("morgan");
+const countriesRouter = require("./countriesRouter");
+const activitiesRouter = require("./activitiesRouter");
 
 const router = Router();
+
+//MIDDLEWARES --> para hacer validaciones tambien modularizo algunos
+router.use(express.json());
+router.use(morgan("dev"));
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+//ROUTERS ---> modularizados
+router.use("/countries", countriesRouter);
+router.use("/activities", activitiesRouter);
 
 module.exports = router;
