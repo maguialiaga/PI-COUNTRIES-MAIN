218d5402a62cdf80a21f314e7fb25a92276592b7
maguialiaga <maguialiaga1@gmail.com>
2022-12-27 18:50:28 -0300

    changes main

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./views/Home/Home";
+// import NavBar from "./components/NavBar/Navbar";
+import LandingPage from "./components/Landing/LandingPage";
+// import Error from "./components/Error/Error";
+// import FormActivity from "./components/Form/FormActivity";
+// import Detail from "./components/Detail/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <>
+      <Switch>
+        {/* <Route path="/home">
+          <NavBar />
+        </Route>
+        <Route exact path="/home/createActivity" component={FormActivity} />
+        <Route exact path="/:countryId" component={Detail} /> */}
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/" component={LandingPage} />
+        {/* <Route path="/" component={Error} />  */}
+      </Switch>
+    </>
   );
 }
 
