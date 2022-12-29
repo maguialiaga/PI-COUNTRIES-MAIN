5fc800fc90666de110164e21569ff0fce2f1fbf1
maguialiaga <maguialiaga1@gmail.com>
2022-12-29 01:18:33 -0300

    change component files to views

--- client/src/components/Detail/ActivityDetail.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ActivityDetail() {
-  return <div></div>;
-}
-
-export default ActivityDetail;
--- client/src/components/Form/FormActivity.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FormActivity() {
-  return <div></div>;
-}
-
-export default FormActivity;
