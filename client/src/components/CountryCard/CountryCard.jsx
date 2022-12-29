24310df982048f54e4378b75b24edc0b7c6aa1a2
maguialiaga <maguialiaga1@gmail.com>
2022-12-29 14:52:39 -0300

    added link to card detail

--- client/src/components/CountryCard/CountryCard.jsx
+++ client/src/components/CountryCard/CountryCard.jsx
@@ -1,16 +1,19 @@
 //COMPONENTE PRESENTACIONAL --> dumb component
 //Tiene que tener las menos tareas posibles, solamente va a mostrar!
 import React from "react";
-import style from "./CountryCard.module.css";
+import styles from "./CountryCard.module.css";
+import { Link } from "react-router-dom";
 
-function CountryCard({ name, image, continent }) {
+function CountryCard({ name, image, continent, id }) {
   //le hago destructuring de mi objeto de props
   return (
-    <div className={style.card}>
-      <img src={image} alt="img" className={style.img} />
-      <p className={style.name}>{name}</p>
-      <p className={style.continent}>{continent}</p>
-    </div>
+    <Link to={`/home/${id}`} className={styles.card}>
+      <div>
+        <img src={image} alt="img" className={styles.img} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.continent}>{continent}</p>
+      </div>
+    </Link>
   );
 }
 
