2aa2289993eda35ea08d92159b4c7bd9de7c5742
maguialiaga <maguialiaga1@gmail.com>
2022-12-29 14:52:00 -0300

    changes in app

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,24 @@ import "./App.css";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./views/Home/Home";
-// import NavBar from "./components/NavBar/Navbar";
 import LandingPage from "./views/Landing/LandingPage";
-// import Error from "./components/Error/Error";
-// import FormActivity from "./components/Form/FormActivity";
-// import Detail from "./components/Detail/Detail";
+import Error from "./views/Error/Error";
+import FormActivity from "./views/Form/FormActivity";
+import Detail from "./views/Detail/Detail";
 
 function App() {
   return (
     <>
       <Switch>
-        {/* <Route path="/home">
-          <NavBar />
-        </Route>
-        <Route exact path="/home/createActivity" component={FormActivity} />
-        <Route exact path="/:countryId" component={Detail} /> */}
+        <Route path="/home/createActivity" component={FormActivity} />
+
+        <Route exact path="/home/:id" component={Detail} />
+
         <Route exact path="/home" component={Home} />
+
         <Route exact path="/" component={LandingPage} />
-        {/* <Route path="/" component={Error} />  */}
+
+        <Route path="/" component={Error} />
       </Switch>
     </>
   );
