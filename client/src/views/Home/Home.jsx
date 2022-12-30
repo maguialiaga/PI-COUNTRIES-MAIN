dd5b39ae294ea005e80910e29289c508e245e572
maguialiaga <maguialiaga1@gmail.com>
2022-12-29 21:23:33 -0300

    changes

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 //import styles from "./Home/Home.modules.css"
-//import { Link } from "react-router-dom";
+//import { NavLink } from "react-router-dom";
 //import { getAllCountries } from "../../redux/actions";
 import CountryCard from "../../components/CountryCard/CountryCard";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import { useDispatch, useSelector } from "react-redux";
 
 function Home() {
+  // const dispatch = useDispatch();
+  //Me traigo del estado global:
+  const countries = useSelector((state) => {});
+
   return (
     <div>
       <SearchBar />
