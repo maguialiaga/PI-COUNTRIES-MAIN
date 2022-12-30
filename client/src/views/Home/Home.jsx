5ca08a825a575b99d9380c6b0daaa21a9cbf1a18
maguialiaga <maguialiaga1@gmail.com>
2022-12-30 14:56:17 -0300

    home process

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
-import { getAllCountries } from "../../redux/actions";
+import {
+  getAllCountries,
+  orderByAlphabet,
+  orderByPopulation,
+  filterByContinent,
+  filterByActivity,
+  getActivity,
+} from "../../redux/actions";
 
 import CountryCard from "../../components/CountryCard/CountryCard";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import Loading from "../../components/Loading/Loading";
 import Paginado from "../../components/Paginado/Paginado";
-import styles from "../Home/Home.modules.css";
-import world from "../../styles/Images";
-import refresh from "../../styles/Images";
+import styles from "../Home/Home.module.css";
+//import world from "../../styles/Images/world.png";
+//import refresh from "../../styles/Images/refresh.png";
 
 function Home() {
   const dispatch = useDispatch();
@@ -21,11 +28,16 @@ function Home() {
 
   //Ahora creo los estados locales que tambien puedo pasar como props
   const [currentPage, setCurrentPage] = useState(1); //pagina actual
-  const [countryPerPage, setCountryPerPage] = useState(9); //cantidad de paises por pagina
-  const [order, setOrder] = useState("");
+  const [countryPerPage] = useState(9); //cantidad de paises por pagina
+
+  useEffect(() => {
+    dispatch(getAllCountries()); //componentDidMount y componentDidUpdate
+    dispatch(getActivity());
+  }, [dispatch]);
 
   //----------------------------------------------------------------------------------------------------------
   //Para paginado
+
   //indice del ultimo pais por pagina
   const lastCountry = (currentPage) => {
     if (currentPage === 1) return 9;
@@ -36,9 +48,9 @@ function Home() {
     if (currentPage === 1) return 0;
     return lastCountry - countryPerPage;
   };
+
   //Arreglo de paises por pagina
   const currentCountries = totalCountries.slice(firstCountry, lastCountry);
-
   const paginado = (pageNum) => setCurrentPage(pageNum);
 
   //----------------------------------------------------------------------------------------------------------
@@ -46,13 +58,29 @@ function Home() {
     dispatch(getAllCountries());
   };
 
-  const handleOrderAlph = () => {};
+  const handleOrderAlph = (e) => {
+    const value = e.target.value;
+    dispatch(orderByAlphabet(value));
+    setCurrentPage(1);
+  };
 
-  const handleOrderPopu = () => {};
+  const handleOrderPopu = (e) => {
+    const value = e.target.value;
+    dispatch(orderByPopulation(value));
+    setCurrentPage(1);
+  };
 
-  const handleFilterCont = () => {};
+  const handleFilterCont = (e) => {
+    const value = e.target.value;
+    dispatch(filterByContinent(value));
+    setCurrentPage(1);
+  };
 
-  const handleFilterAct = () => {};
+  const handleFilterAct = (e) => {
+    const value = e.target.value;
+    dispatch(filterByActivity(value));
+    setCurrentPage(1);
+  };
 
   //----------------------------------------------------------------------------------------------------------
   return (
@@ -61,7 +89,7 @@ function Home() {
         <div className={styles.navBar}>
           <div className={styles.navIzq}>
             <Link to="/">
-              <img src={world} alt="world" className={styles.worldImg}></img>
+              <p>Go back</p>
             </Link>
           </div>
 
@@ -74,7 +102,8 @@ function Home() {
           </div>
 
           <button onClick={handleClick()} className={styles.buttonRef}>
-            <img src={refresh} alt="refresh" className={styles.refresh}></img>
+            {/* <img src={refresh} alt="refresh" className={styles.refresh}></img> */}
+            Refresh
           </button>
 
           <div className={styles.filters}>
@@ -82,7 +111,7 @@ function Home() {
               onChange={(e) => handleOrderAlph(e)}
               className={styles.order}
             >
-              <option value="all">Order</option>
+              <option value="All">Order</option>
               <option value="asc">A-Z</option>
               <option value="desc">Z-A</option>
             </select>
@@ -90,7 +119,7 @@ function Home() {
               onChange={(e) => handleOrderPopu(e)}
               className={styles.div_selectAll}
             >
-              <option value="all">All</option>
+              <option value="All">All</option>
               <option value="higher">Higher</option>
               <option value="lower">Lower</option>
             </select>
@@ -98,7 +127,7 @@ function Home() {
               onChange={(e) => handleFilterCont(e)}
               className={styles.div_selectAll}
             >
-              <option value="all">All</option>
+              <option value="All">All</option>
               <option value="north america">North America</option>
               <option value="south america">South America</option>
               <option value="africa">Africa</option>
@@ -111,37 +140,40 @@ function Home() {
               onChange={(e) => handleFilterAct(e)}
               className={styles.created}
             >
-              <option value="all">Activities</option>
+              <option value="All">Activities</option>
               {totalAct.map((act) => {
                 return <option value={act.name}>{act.name}</option>;
               })}
             </select>
           </div>
-          <div className={styles.div_countries}>
-            {currentCountries > 0 ? (
-              currentCountries.map((country) => {
-                return (
-                  <CountryCard
-                    name={country.name}
-                    image={country.image}
-                    continent={country.continent}
-                    id={country.id}
-                  />
-                );
-              })
-            ) : countriesCopy > 0 ? (
-              <Loading />
-            ) : (
-              <div>
-                <p>
-                  <b>There are no countries with those features</b>
-                </p>
-              </div>
-            )}
-          </div>
-          <div className={styles.pagi}>
-            <Paginado allCountries={totalCountries.length} />
-          </div>
+        </div>
+        <div className={styles.div_countries}>
+          {currentCountries > 0 ? (
+            currentCountries.map((country) => {
+              return (
+                <CountryCard
+                  name={country.name}
+                  image={country.image}
+                  continent={country.continent}
+                  id={country.id}
+                />
+              );
+            })
+          ) : countriesCopy < 0 ? (
+            <Loading />
+          ) : (
+            <div>
+              <p>
+                <b>There are no countries with those features</b>
+              </p>
+            </div>
+          )}
+        </div>
+        <div className={styles.pagi}>
+          <Paginado
+            totalCountries={totalCountries.length}
+            paginado={paginado}
+          />
         </div>
       </div>
     </div>
