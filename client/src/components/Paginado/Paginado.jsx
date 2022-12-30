c5c57549a94a4986472335ae7163492210f2245e
maguialiaga <maguialiaga1@gmail.com>
2022-12-30 14:56:54 -0300

    paginado process

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,18 +1,30 @@
 //dividir cantidad de paginas x cantdidad de CountryCards
 //si el modulo no da exacto osea 18 % 4 > 1 ---> 4 + 1 = 5 paginas, para mostrar los elementos que queden restantes
-//voy a hacer el redondeo con ceil voy ---> para ver si me toma
-//10 paises por pagina
+//voy a hacer el redondeo con ceil ---> para ver si me toma + 1 si me llega a sobrar paises
+//10 paises por pagina ---> la primera arranca con 9
 //guardar en una variable el numero de paginas
 
 import React from "react";
+import styles from "../Paginado/Paginado.module.css";
 
-function Paginado(props) {
+function Paginado({ paginado, totalCountries }) {
   const pageNumbers = [];
-  for (let i = 0; i < Math.ceil(props.countries / 9); i++) {
+
+  for (let i = 0; i <= Math.ceil(totalCountries / 9); i++) {
     pageNumbers.push(i);
   }
 
-  return <div></div>;
+  return (
+    <nav className={styles.navPag}>
+      <ul className={styles.ulPag}>
+        {pageNumbers?.map((number) => (
+          <li className={styles.liPag} key={number}>
+            <button onClick={() => paginado(number)}>{number}</button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Paginado;
