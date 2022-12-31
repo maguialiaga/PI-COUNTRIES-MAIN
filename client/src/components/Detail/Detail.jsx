078fcf1f9f4d37d14b103e391700af79ee07da65
maguialiaga <maguialiaga1@gmail.com>
2022-12-31 19:25:54 -0300

    begining with detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../Detail/Detail.module.css";
+import back from "../../styles/Images/atras.png";
+
+import CountryDetail from "./CountryDetail";
+import ActivityDetail from "./ActivityDetail";
 
 function Detail() {
-  return <div></div>;
+  return (
+    <div className={styles.contPag}>
+      <div>
+        <div className={styles.navBar}>
+          <div className={styles.navIzq}>
+            <NavLink to="/home">
+              <img src={back} alt="back" className={styles.arrowBack}></img>
+            </NavLink>
+          </div>
+        </div>
+        <div className={styles.contGral}>
+          <div className={styles.cardForm}>
+            <CountryDetail />
+          </div>
+          <div className={styles.cardForm}>
+            <ActivityDetail />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
